73a07faab3700eaa7121d972f0528e2cd5092ec7
Aleksandr <sheph@ukr.net>
2021-07-28 20:54:15 +0300

    added a pidgotovka page

--- app/js/main.js
+++ app/js/main.js
@@ -30,5 +30,16 @@ $(function () {
 
 
 
+    $(function () {
+        $('.jqueryOptions').hide();
+
+        $('#choose').change(function () {
+            $('.jqueryOptions').slideUp();
+            $('.jqueryOptions').removeClass('current-opt');
+            $("." + $(this).val()).slideDown();
+            $("." + $(this).val()).addClass('current-opt');
+        });
+    });
+
 
 });
